be7f1d00a5b047e23bda419f49f7c93e55adc5d3
luciana.lucio <luciana.lucio@g.globo>
2025-02-19 19:08:06 -0300

    creating Navbar and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 //pages
 import About from "./pages/About/About";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 
 import "./App.css";
 
@@ -10,12 +12,14 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
