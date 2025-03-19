e97c68ada6245c95c1ef7f1e1a3015e06c96d4d7
luciana.lucio <luciana.lucio@g.globo>
2025-03-19 19:30:38 -0300

    links for authenticated users

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import { AuthProvider } from "./context/AuthContext";
 import { useAuthentication } from "./hooks/useAuthentication";
 //pages
 import About from "./pages/About/About";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
@@ -43,6 +45,8 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/posts/create" element={<CreatePost />} />
+              <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
           </div>
           <Footer />
