563a0ee4eafc52a2f60e1143aadbbcd51b72c699
luciana.lucio <luciana.lucio@g.globo>
2025-04-01 09:40:06 -0300

    structuring the dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,33 @@
+import { Link } from "react-router-dom";
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  // posts do usuário
+  const posts = [];
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      )}
     </div>
   );
 };
