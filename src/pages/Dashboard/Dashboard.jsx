3196a0cf85f2c459d487a2a21cbca8660e19d2a1
luciana.lucio <luciana.lucio@g.globo>
2025-04-02 09:20:44 -0300

    completing the dashboard structure

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -11,6 +11,12 @@ const Dashboard = () => {
 
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
+  const deleteDocument = (id) => {};
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div>
       <h2>Dashboard</h2>
@@ -23,11 +29,37 @@ const Dashboard = () => {
           </Link>
         </div>
       ) : (
-        <div>
-          <p>Tem posts!</p>
-        </div>
+        <>
+          <div>
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
+
+          {posts &&
+            posts.map((post) => (
+              <div key={post.id}>
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                    Ver
+                  </Link>
+                  <Link
+                    to={`/posts/edit/${post.id}`}
+                    className="btn btn-outline"
+                  >
+                    Editar
+                  </Link>
+                  <button
+                    onClick={() => deleteDocument(post.id)}
+                    className="btn btn-outline btn-danger"
+                  >
+                    Excluir
+                  </button>
+                </div>
+              </div>
+            ))}
+        </>
       )}
-      {posts && posts.map((post) => <h3>{post.title}</h3>)}
     </div>
   );
 };
